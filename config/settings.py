e2c1021f4a5a91a1aee00db53a9cd5ffd493eb0f
o1-mat <168581110+o1-mat@users.noreply.github.com>
2024-12-16 08:58:55 +0900

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -29,9 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [
-    os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
