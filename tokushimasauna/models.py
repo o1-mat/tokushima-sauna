5c395baeeabfe4605c584eddb12f60c7d3c45482
o1-mat <168581110+o1-mat@users.noreply.github.com>
2024-12-29 04:55:15 +0900

    Add files via upload

--- tokushimasauna/models.py
+++ tokushimasauna/models.py
@@ -5,7 +5,8 @@ from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=200, verbose_name='名称')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='作成日')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='登録日')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日")
 
     def __str__(self):
         return self.name
@@ -23,10 +24,11 @@ class Spa(models.Model):
     catchphrase = models.TextField(blank=True, null=True, verbose_name='キャッチフレーズ')
     address = models.CharField(max_length=200, verbose_name='住所')
     tel = models.CharField(max_length=20, verbose_name='電話番号')
-    price = models.PositiveIntegerField(verbose_name="料金設定", default=0)
+    price = models.PositiveIntegerField(verbose_name="基本料金", default=0)
     holiday = models.CharField(max_length=50, blank=True, null=True, verbose_name='定休日')
     operating_hours = models.CharField(null=True, blank=True, max_length=100, verbose_name='営業時間')
-    created_at = models.DateTimeField(auto_now_add=True, max_length=50, verbose_name='作成日')
+    created_at = models.DateTimeField(auto_now_add=True, max_length=50, verbose_name='登録日')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日")
     favorited_by = models.ManyToManyField(User, related_name='favorites', blank=True, verbose_name='お気に入り')
     
     def __str__(self):
@@ -47,9 +49,9 @@ class Favorite(models.Model):
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="ユーザー")
     spa = models.ForeignKey(Spa, on_delete=models.CASCADE, verbose_name="施設")
-    star = models.IntegerField(verbose_name="評価", choices=[(i, i) for i in range(1, 6)])
-    content = models.TextField(verbose_name="レビュー内容")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日")
+    star = models.IntegerField(verbose_name="星の数", choices=[(i, i) for i in range(1, 6)])
+    content = models.TextField(verbose_name="コメント")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="登録日")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日")
 
     def __str__(self):
